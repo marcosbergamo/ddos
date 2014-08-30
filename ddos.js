02fb936bb5667002d589e67be56ca73e783766c3
Markus Kefka's <markusdarkness@gmail.com>
2014-08-30 10:37:30 -0300

    Commander

--- ddos.js
+++ ddos.js
@@ -1,9 +1,26 @@
 #!/usr/bin/env node
 
-var args = process.argv.slice(2);
+var program = require('commander');
+
+function list(val) {
+  return val.split(',');
+}
+
+program
+.version('0.1.0')
+.option('hello <s>', 'Diga seu nome', String)
+.option('maior <items>', 'Retorna o maior numero', list)
+.parse(process.argv);
+
 
 function maior(num1, num2){
   return num1 > num2 ? num1 : num2;
 }
 
-console.log(maior(args[0], args[1]));
+if(program.hello){
+  console.log("Hello "+program.hello);
+}
+
+if(program.maior){
+  console.log(maior(program.maior[0], program.maior[1]));
+}

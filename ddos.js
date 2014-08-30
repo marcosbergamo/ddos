02bbaa355e75728e2fdc200b4c112cfc1270867f
Markus Kefka's <markusdarkness@gmail.com>
2014-08-30 09:42:47 -0300

    hello Marcos

--- ddos.js
+++ ddos.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+var args = process.argv;
+
+console.log('Seus argumentos: '+args);
+
+var args2 = args.slice(2);
+
+console.log('hello '+args2[0]);

